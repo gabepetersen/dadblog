3adf51536b00c47a18ecffe4aef9ef99fbb85840
gabepetersen <precariousplant@gmail.com>
2022-08-27 19:39:01 -0400

    Style up the fancy create page
    This is actually so cool I love it

--- pages/create.tsx
+++ pages/create.tsx
@@ -57,27 +57,27 @@ export default function Create() {
 
   return (
     <Layout>
-      <h1>Write Post</h1>
-      <p>For the new Mutual Threads update, I have decided to use <Link href="https://github.com/showdownjs/showdown/wiki">Showdown</Link> to give y&apos;all a little more flexibility in posting content. Please read <Link href="https://github.com/showdownjs/showdown/wiki/Showdown's-Markdown-syntax">Showdown Markdown Syntax</Link> on how to format your blog post. Along this documentation, keep in mind that most of the settings are enabled, so feel free to use auto-links, sized images, emojis, etc.</p>
-
-      <h2>Content Rules</h2>
-      <ul>
-        <li>No adult or hate content.</li>
-        <li>Provide warnings for stuff like excessive profanity</li>
-        <li>If you add special characters in the title, they will be removed from the post&apos;s URL.</li>
-        <li>I would advise against using H1 (# header) tags inside the content since the title is already formatted as an H1.</li>
-        <li>If you don&apos;t like how your post looks or you want to delete it - reach out to me. My hope is to eventually implement draft functionality.</li>
-      </ul>
-      <form className={styles.blogForm} onSubmit={handleSubmit}>
-        <label htmlFor="btitle">Title</label>
-        <br />
-        <input type="text" id="btitle" name="btitle" onChange={handleTitle} /><br />
-        <label htmlFor="btext">Content</label>
-        <br />
-        <textarea id="btext" name="btext" onChange={handleText}></textarea><br />
-        <input type="submit" value="Post" />
-      </form>
-      <br />
+      <section>
+        <h1 className='headingLg'>Write a Post</h1>
+        <p>For the new Mutual Threads update, I have decided to use <Link href="https://github.com/showdownjs/showdown/wiki">Showdown</Link> to give y&apos;all a little more flexibility in posting content. Please read <Link href="https://github.com/showdownjs/showdown/wiki/Showdown's-Markdown-syntax">Showdown Markdown Syntax</Link> on how to format your blog post. Along this documentation, keep in mind that most of the settings are enabled, so feel free to use auto-links, sized images, emojis, etc.</p>
+        <h2>Content Rules</h2>
+        <ul>
+          <li>No adult or hate content.</li>
+          <li>Provide warnings for stuff like excessive profanity</li>
+          <li>If you add special characters in the title, they will be removed from the post&apos;s URL.</li>
+          <li>I would advise against using H1 (# header) tags inside the content since the title is already formatted as an H1.</li>
+          <li>If you don&apos;t like how your post looks or you want to delete it - reach out to me. My hope is to eventually implement draft functionality.</li>
+        </ul>
+      </section>
+      <section>
+        <form className={styles.blogForm} onSubmit={handleSubmit}>
+          <label htmlFor="btitle">Title</label>
+          <input type="text" id="btitle" name="btitle" onChange={handleTitle} /><br />
+          <label htmlFor="btext">Content</label>
+          <textarea id="btext" name="btext" onChange={handleText}></textarea><br />
+          <input type="submit" value="Post" />
+        </form>
+      </section>
       <ToastContainer />
     </Layout>
   );
